a14d2684f6ecb7bab5fec9259936a3e3e7658584
Memeldu72 <63606977+Memeldu72@users.noreply.github.com>
2021-04-09 21:05:52 +0200

    Add files via upload

---
+++ ReceleurV2/config.lua
@@ -0,0 +1,19 @@
+Config = {
+    Item = {
+        Banque = {
+            {name = "goldwatch", label = "Montre en Or", price = 5000},
+            {name = "goldnecklace", label = "Collier en or", price = 600},
+            {name = "goldbar", label = "Lingot d'or", price = 700},
+            {name = "diamond", label = "Diamant", price = 20000}
+        },
+
+        Bijouterie =  {
+            {name = "jewels", label = "Bijoux", price = 2500}
+        }
+    },
+
+    Position = {
+        {pos = vector3(-442.896, 2015.3698, 123.545), pedPos = vector3(-442.896, 2015.3698, 122.545), heading = 112.55, pedModel = "g_m_m_chicold_01"}
+    }
+
+}
