72028cd065834e2651f99a7bc4eafbde0c4ea575
Memeldu72 <63606977+Memeldu72@users.noreply.github.com>
2021-11-18 17:59:08 +0100

    Update fxmanifest.lua

--- ReceleurV2/fxmanifest.lua
+++ ReceleurV2/fxmanifest.lua
@@ -7,7 +7,7 @@ client_scripts {
     "src/menu/Menu.lua",
     "src/menu/MenuController.lua",
     "src/components/*.lua",
-    "src/menu/elements/*.lua"
+    "src/menu/elements/*.lua",
     "src/menu/items/*.lua",
     "src/menu/panels/*.lua",
     "src/menu/windows/*.lua"
